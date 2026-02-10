758345c3e22bf971387b62b7a1518fff67f28f23
Michele <michele.saia1996@gmail.com>
2026-02-10 12:46:27 +0100

    ticket:3 implementazione del  2° e 3° punto

--- index.js
+++ index.js
@@ -2,39 +2,96 @@
 
 
 const accounts =[]
-let User1={
-    Username:"Baolo",
-    Pin: "ciao" ,
+let user1={
+    username:"Baolo",
+    pin: "ciao",
     balance: 500,
-    Movements: []
+    movements: [],
+    // controllo disponibilità fondi 
+    Withdrawal: function() {  
+       
+    }
 }
 
-let User2={
-    Username: "BrunoH",
-    Pin: "<18 <3"  ,
-    balance: 10000  ,
-    Movements:[]  
+let user2={
+    username: "BrunoH",
+    pin: "<18 <3",
+    balance: 10000,
+    movements:[]  
 }
 
-accounts.splice(0,0,User1,User2)
+accounts.splice(0,0,user1,user2)
 console.log(accounts);
 
-let UserLogin=prompt('Enter your Username');
-let PinLogin=prompt('Enter your Private Password');
+let userLogin=prompt('Enter your Username:');
+let pinLogin=prompt('Enter your Private Password:');
 
 
 for( let obj of accounts){
-    if (UserLogin===obj.Username && PinLogin===obj.Pin) {
-        alert("Welcome back hero!");
-        
-        
-       
-    }else{
+    if (userLogin === obj.username){
+        if(pinLogin === obj.pin) {
+            alert("Welcome back!");
+            const inputMenu = prompt(' 1. Withdrawal \n 2. Deposite \n 3. CheckBalance \n 4. History ')
+            if (inputMenu == 1) {
+                let amount = prompt('How much you want Withdrawal?');
+                if (amount <= obj.balance && amount>0 ) {
+                    obj.balance-=amount;
+                    alert('Operation confirmed, good joob champ !');
+                    let operation= {
+                        type: "withdrawal",
+                        cash: amount,
+                        date : new Date()
+                    
+                    }
+                    obj.movements.push(operation);
+                }else{
+                    alert('Try again furbacchione!')
+                }
+                
+
+
+
+
+            
+            } else if(inputMenu == 2 ){
+                let bankPayment = parseInt(prompt('Enter how much you want add in your Balance:'));
+                if (bankPayment > 0  ) {
+                    obj.balance += bankPayment;
+                    alert(`Balance Update : £ ${obj.balance.toFixed(2)} `);
+                    let operation= {
+                        type: "payment",
+                        cash: bankPayment,
+                        date : new Date()
+                    
+                    }
+                    obj.movements.push(operation);
+                    
+
+                    
+                }
+
+
+
+            } else if(inputMenu == 3){
+                alert(obj.movements) // fin quando non viene fatto il ciclo , non stamperà nessun movimento salvato
+            
+
+                
+
+            } else if (inputMenu == 4){
+
+            }
+            break;
+        } else {
+            alert('fail... try again loser!')
+            break;
+        } 
+    }
+    if(accounts.indexOf(obj) == accounts.length-1){
         alert('fail... try again loser!')
     }
-    
-    
-    
 }
+
+
 
 
